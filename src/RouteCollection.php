65146fadce51420e067350be6f5e9107b7a1f909
James Cavaliere <james.cavaliere@gmail.com>
2025-09-26 09:00:32 -0400

    Init

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -8,12 +8,22 @@ use RecursiveIteratorIterator;
 
 class RouteCollection
 {
-    private array $routes = [];
+    private array        $routes = [];
+    private RouterConfig $router_config;
 
-    public function __construct()
+    public function __construct(RouterConfig $router_config)
     {
+        $this->router_config = $router_config;
     }
 
+    /**
+     * Matches the given HTTP method and URI to a route.
+     *
+     * @param string $http_method The HTTP method (e.g., GET, POST) used for the request.
+     * @param string $uri         The URI of the request to match against defined routes.
+     *
+     * @return Route|false Returns the matched Route object if found, or false if no route is matched.
+     */
     public function matchRoute(string $http_method, string $uri): Route|false
     {
         /** @var Route $route */
@@ -33,7 +43,13 @@ class RouteCollection
         return false;
     }
 
-    public function processClassPaths(array $paths, int $fqcn_depth = 1000): void
+    /**
+     * @param array $paths
+     *
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function processClassPaths(array $paths): void
     {
         foreach ($paths as $path) {
             if (is_dir($path)) {
@@ -41,7 +57,7 @@ class RouteCollection
                 $files = new RecursiveIteratorIterator($di);
                 foreach ($files as $file) {
                     $file_path = $file->getPathname();
-                    if ($class = Router::getFQCNFromFile($file_path, $fqcn_depth)) {
+                    if ($class = $this->getFQCNFromFile($file_path)) {
                         $reflect = new \ReflectionClass($class);
                         $c_attributes = $reflect->getAttributes(CRoute::class);
                         if (!empty($c_attributes)) {
@@ -67,6 +83,46 @@ class RouteCollection
                 }
             }
         }
+    }
+
+    /**
+     * Retrieves the Fully Qualified Class Name (FQCN) from a PHP file by parsing its contents.
+     *
+     * @param string $filepath The absolute or relative path to the PHP file to parse.
+     *
+     * @return string|false The FQCN of the class if found, otherwise false.
+     */
+    public function getFQCNFromFile(string $filepath): string|false
+    {
+        $namespace = null;
+        $class = null;
+        $contents = file_get_contents($filepath, length: $this->router_config->getFQCNFileDepth());
+        $tokens = token_get_all($contents);
+        $i = 0;
+        while ($i < count($tokens) && $i < 50) {
+            if (is_array($tokens[$i]) && $tokens[$i][0] === T_NAMESPACE) {
+                // Find the namespace declaration
+                $i++; //Move past the T_NAMESPACE
+                while (isset($tokens[$i]) && is_array($tokens[$i]) && $tokens[$i][0] === T_WHITESPACE) {
+                    $i++; // Skip Whitespace
+                }
+                $namespace = $tokens[$i][1];
+
+            } elseif (is_array($tokens[$i]) && $tokens[$i][0] === T_CLASS) {
+                // Find the class declaration
+                $i++; // Move past T_CLASS
+                while (isset($tokens[$i]) && is_array($tokens[$i]) && $tokens[$i][0] === T_WHITESPACE) {
+                    $i++; // Skip whitespace
+                }
+                $class = $tokens[$i][1];
+
+            }
+            $i++;
+            if (isset($namespace, $class)) {
+                return "\\$namespace\\$class";
+            }
+        }
+        return false;
     }
 
     public function addRoute(string $method, Route $route, int $priority = 999): void
--- src/Router.php
+++ src/Router.php
@@ -11,50 +11,10 @@ class Router
     public function __construct(RouterConfig $router_config)
     {
         $this->router_config = $router_config;
-        $this->route_collection = new RouteCollection();
+        $this->route_collection = new RouteCollection($router_config);
         if (!empty($paths = $this->router_config->getPaths())) {
             $this->route_collection->processClassPaths($paths, $this->router_config->getFQCNFileDepth());
         }
-    }
-
-    /**
-     * Retrieves the Fully Qualified Class Name (FQCN) from a PHP file by parsing its contents.
-     *
-     * @param string $filepath The absolute or relative path to the PHP file to parse.
-     *
-     * @return string|false The FQCN of the class if found, otherwise false.
-     */
-    public static function getFQCNFromFile(string $filepath, int $length = 1000): string|false
-    {
-        $namespace = null;
-        $class = null;
-        $contents = file_get_contents($filepath, length: $length);
-        $tokens = token_get_all($contents);
-        $i = 0;
-        while ($i < count($tokens) && $i < 50) {
-            if (is_array($tokens[$i]) && $tokens[$i][0] === T_NAMESPACE) {
-                // Find the namespace declaration
-                $i++; //Move past the T_NAMESPACE
-                while (isset($tokens[$i]) && is_array($tokens[$i]) && $tokens[$i][0] === T_WHITESPACE) {
-                    $i++; // Skip Whitespace
-                }
-                $namespace = $tokens[$i][1];
-
-            } elseif (is_array($tokens[$i]) && $tokens[$i][0] === T_CLASS) {
-                // Find the class declaration
-                $i++; // Move past T_CLASS
-                while (isset($tokens[$i]) && is_array($tokens[$i]) && $tokens[$i][0] === T_WHITESPACE) {
-                    $i++; // Skip whitespace
-                }
-                $class = $tokens[$i][1];
-
-            }
-            $i++;
-            if (isset($namespace, $class)) {
-                return "\\$namespace\\$class";
-            }
-        }
-        return false;
     }
 
     public function getRoute(?string $uri = null, ?string $http_method = null): Route|null
