01b872443672bc29d12f205f04cbeb0ad2eca1cf
James Cavaliere <james.cavaliere@gmail.com>
2025-09-26 12:11:58 -0400

    Init

--- src/MRoute.php
+++ src/MRoute.php
@@ -7,7 +7,7 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class MRoute
 {
-    public function __construct(string $m_route,
+    public function __construct(string $route,
                                 string $http_method = 'get',
                                 int    $priority = 999,
                                 bool   $protected = true,)
