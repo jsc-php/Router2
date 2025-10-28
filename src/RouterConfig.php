08e51883ace1c5aed1492cf8f418a2a7aaa94fb3
James Cavaliere <james.cavaliere@gmail.com>
2025-10-28 10:36:08 -0400

    Dev

--- src/RouterConfig.php
+++ src/RouterConfig.php
@@ -4,35 +4,15 @@ namespace JscPhp\Router2;
 class RouterConfig
 {
     private array  $paths            = [];
-    private bool   $protect_default  = false;
-    private string $protect_redirect = '';
     private bool   $dev              = false;
 
     public function __construct()
     {
     }
 
-    public function getProtectRedirect(): string
-    {
-        return $this->protect_redirect;
-    }
 
-    public function setProtectRedirect(string $protect_redirect): RouterConfig
-    {
-        $this->protect_redirect = $protect_redirect;
-        return $this;
-    }
 
-    public function isProtectDefault(): bool
-    {
-        return $this->protect_default;
-    }
-
-    public function setProtectDefault(bool $protect_default): RouterConfig
-    {
-        $this->protect_default = $protect_default;
-        return $this;
-    }
+  
 
     public function isDev(): bool
     {
